f0970658f851b864595c8147ba261f0ef60c28be
Андрей Осинцев <asosintsev@yandex.ru>
2025-03-20 19:47:52 +0400

    feat: all layout done

--- images/products/netz-xo/scripts/init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function setFullHeight() {
-        const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-      }
-      
-    setFullHeight();
-    window.addEventListener('resize', setFullHeight);
-});
